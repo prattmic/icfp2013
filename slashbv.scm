478f01e997985f60d77783d854455a3830b1ef9d
Michael Pratt <michael@pratt.im>
2013-08-09 00:17:03 -0700

    Implement fold

--- slashbv.scm
+++ slashbv.scm
@@ -4,6 +4,18 @@
 (define (uint64 x)
  (bitwise-and #xFFFFFFFFFFFFFFFF x))
 
+; List all bytes in a uint64
+(define (byte-list x)
+ (list
+  (arithmetic-shift (bitwise-and x #xff) 0)
+  (arithmetic-shift (bitwise-and x #xff00) -8)
+  (arithmetic-shift (bitwise-and x #xff0000) -16)
+  (arithmetic-shift (bitwise-and x #xff000000) -24)
+  (arithmetic-shift (bitwise-and x #xff00000000) -32)
+  (arithmetic-shift (bitwise-and x #xff0000000000) -40)
+  (arithmetic-shift (bitwise-and x #xff000000000000) -48)
+  (arithmetic-shift (bitwise-and x #xff00000000000000) -56)))
+
 (define (not x)
  (uint64 (bitwise-not (uint64 x))))
 
@@ -34,5 +46,8 @@
 (define (if0 t x y)
  (if (= t 0) x y))
 
+(define (fold x init op)
+ (foldl op init (byte-list (uint64 x))))
+
 ; Test
-(display (if0 0 0 1))
+(display ((lambda (x) (fold x 0 (lambda (x y) (plus x y)))) #x0201))
